5190cb33ad235057b0218ff55eaa81de73313ef5
BuildTools <joshw096@gmail.com>
2020-08-16 21:22:49 +0100

    Count words and elons

--- processor.js
+++ processor.js
@@ -11,46 +11,27 @@ async function processData() {
     console.info("Starting Data Processing...");
     const filenames = await readDir();
     data = await readFiles(filenames);
-    let wordcount = 0;
     data.forEach((d, i) => {
         const parsed = JSON.parse(d);
-        wordcount += parsed.m_iDocBodyWordCnt;
         cache.set(i, parsed);
     });
+    const wordCount = await countWords(data);
     const yearData = await getSourcesFromYear(data);
     const countryData = await getCountries(data);
-    const countryYear = await getCountryYears(data);
+    const countryYears = await getCountryYears(data);
+    const elonCount = await getElon(data);
     const overview = {
         yearData,
         countryData,
+        countryYears,
+        wordCount,
+        elonCount,
     };
     cache.set("overview", overview);
-    cache.set("words", wordcount);
+    cache.set("words", wordCount);
     console.info(cache.getStats());
     console.info("Data Processing Complete - Cache SET");
 }
-
-const getArticle = (req, res) => {
-    const { query } = req.query;
-    res.send(data[query]);
-};
-
-const getOverview = async (req, res) => {
-    try {
-        const yearData = await getSourcesFromYear(data);
-        const countryData = await getCountries(data);
-        const overview = {
-            yearData,
-            countryData,
-        };
-        res.send(overview);
-        return;
-    } catch (err) {
-        console.error(err);
-        res.status(500);
-        return;
-    }
-};
 
 const checkCache = (req, res, next) => {
     const { query } = req.query;
@@ -60,6 +41,34 @@ const checkCache = (req, res, next) => {
     } else {
         console.info(`${query} found in cache!`);
         res.send(data);
+        return;
+    }
+};
+
+const getArticle = (req, res) => {
+    const { query } = req.query;
+    res.send(data[query]);
+};
+
+const getOverview = async (req, res) => {
+    try {
+        const wordCount = await countWords(data);
+        const yearData = await getSourcesFromYear(data);
+        const countryData = await getCountries(data);
+        const countryYears = await getCountryYears(data);
+        const elonCount = await getElon(data);
+        const overview = {
+            yearData,
+            countryData,
+            countryYears,
+            wordCount,
+            elonCount,
+        };
+        res.send(overview);
+        return;
+    } catch (err) {
+        console.error(err);
+        res.status(500);
         return;
     }
 };
@@ -95,21 +104,44 @@ async function getSourcesFromYear(data) {
     return years;
 }
 
-const getCountryYears = async () => {
+const getCountryYears = async (data) => {
     let countryYear = {};
     data.forEach((d, i) => {
-        if (i < 3) {
-            const parsed = JSON.parse(d);
-            const year = parsed.m_szYear;
-            if (parsed.m_szGeo1 === "") {
-                parsed.m_szGeo1 = "Unknown Geo";
-            }
-            if (!countryYear[`${parsed.m_szGeo1}`]) {
-                countryYear[`${parsed.m_szGeo1}`] = 1;
+        const parsed = JSON.parse(d);
+        const year = parsed.m_szYear;
+        if (parsed.m_szGeo1 === "") {
+            parsed.m_szGeo1 = "Unknown Geo";
+        }
+
+        if (!countryYear[`${parsed.m_szGeo1}`]) {
+            countryYear[`${parsed.m_szGeo1}`] = [`${parsed.m_szYear}`];
+        } else {
+            if (!countryYear[`${parsed.m_szGeo1}`].includes(`${parsed.m_szYear}`)) {
+                countryYear[`${parsed.m_szGeo1}`].push(`${parsed.m_szYear}`);
             }
         }
     });
     return countryYear;
+};
+
+const countWords = async (data) => {
+    let wordCount = 0;
+    data.forEach((d, i) => {
+        const parsed = JSON.parse(d);
+        const words = parsed.m_iDocBodyWordCnt;
+        wordCount += words;
+    });
+    return wordCount;
+};
+
+const getElon = async (data) => {
+    let elonCount = 0;
+    data.forEach((d, i) => {
+        const parsed = JSON.parse(d);
+        const elon = (parsed.m_szDocBody.match(/elon/g) || []).length;
+        elonCount += elon;
+    });
+    return elonCount;
 };
 
 module.exports = {
